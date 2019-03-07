0d414232e99feab1c8a1f413ac33ae89b4e7d822
sprphnx <sprphnx@outlook.com>
2019-03-08 01:26:14 +0300

    Initial commit

---
+++ src/main/java/com/sprphnx/restws/RestwsApplication.java
@@ -0,0 +1,13 @@
+package com.sprphnx.restws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestwsApplication.class, args);
+	}
+
+}
