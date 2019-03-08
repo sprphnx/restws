d0260f6a340a96c2ec07cb07a0e61441acf30092
sprphnx <sprphnx@outlook.com>
2019-03-08 19:50:49 +0300

    JDBC Security updated

--- src/main/java/com/sprphnx/restws/config/SecurityConfiguration.java
+++ src/main/java/com/sprphnx/restws/config/SecurityConfiguration.java
@@ -1,24 +1,31 @@
 package com.sprphnx.restws.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	DataSource datasource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		UserBuilder users = User.withDefaultPasswordEncoder();
-
-		auth.inMemoryAuthentication().withUser(users.username("rohananoop").password("test123").roles("EMP"));
+		//Reading users and roles from DB
+		auth.jdbcAuthentication().dataSource(datasource);
+		
+		//In memory user creation
+		//UserBuilder users = User.withDefaultPasswordEncoder();
+		//auth.inMemoryAuthentication().withUser(users.username("rohananoop").password("test123").roles("EMP"));
 
 	}
 
